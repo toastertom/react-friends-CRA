28ae65cf6ec4eede2998695b3c35837af629c33a
ToasterTom <tate.thomas5595@gmail.com>
2017-02-28 14:20:38 -0700

    First Commit

---
+++ react-friends/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>The <strong>facebook</strong> Friend Machine</h1>
+
+          <div className="friends">
+
+          </div>
+          
+      </div>
+    );
+  }
+}
+
+export default App;
